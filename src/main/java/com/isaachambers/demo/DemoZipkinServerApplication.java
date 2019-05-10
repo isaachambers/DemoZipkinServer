9cf90cb9b08c3238f432bc4c74db8a18e2ccf6af
isaachambers <isaaceduardk@gmail.com>
2019-05-10 19:44:29 +0300

    first commit

---
+++ src/main/java/com/isaachambers/demo/DemoZipkinServerApplication.java
@@ -0,0 +1,13 @@
+package com.isaachambers.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoZipkinServerApplication.class, args);
+	}
+
+}
